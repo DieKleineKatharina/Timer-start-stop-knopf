73961497c3dedb6584aa8c460af635b980d2ef06
daffiprogs <urlaubkcs@gmx.de>
2016-06-07 16:25:12 +0200

    Simple demonstration of the object Timer

--- timer start stop knopf/Form1.Designer.vb
+++ timer start stop knopf/Form1.Designer.vb
@@ -22,9 +22,34 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(105, 117)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(90, 23)
+        Me.button1.TabIndex = 0
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents button1 As Button
+    Friend WithEvents Timer1 As Timer
 End Class
--- timer start stop knopf/Form1.vb
+++ timer start stop knopf/Form1.vb
@@ -1,3 +1,26 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        button1.Text = "Timer starten"
+        Text = "Starten Sie den Timer"
+    End Sub
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+
+        Timer1.Enabled = Not Timer1.Enabled
+
+        If Timer1.Enabled = False Then
+            'button1.Text = "Timer starten"
+            button1.Text = "Timer starten"
+            Text = "Starten Sie den Timer"
+
+        Else
+
+            button1.Text = "Timer stoppen"
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Text = Date.Now.ToString()
+    End Sub
+
 
 End Class
